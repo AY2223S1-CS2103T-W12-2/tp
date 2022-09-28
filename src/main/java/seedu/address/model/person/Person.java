9f07cc8fe3bf9026a5f7e198c3f7a0d1113a1e76
Neethesh26 <neethesh26@gmail.com>
2022-09-28 12:25:50 +0800

    update

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -25,6 +25,18 @@ public class Person {
     private final Address address;
     private final Set<Tag> tags = new HashSet<>();
 
+    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
+        requireAllNonNull(name, phone, email, address, tags);
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        this.remark = null;
+        this.tags.addAll(tags);
+
+    }
+
+
     /**
      * Every field must be present and not null.
      */
